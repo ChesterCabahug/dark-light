8e5c81f51743e34f6b058e7b1ca6d26c35451fc8
Chester <chesterdeveloper2000@gmail.com>
2020-07-23 17:07:34 +0800

    a more subtle way to refactor

--- script.js
+++ script.js
@@ -6,47 +6,32 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
-
-// helper func for dark mode
-whatMode = (navBgInRGB, textBoxBgInRGB, modeName, removeClass, addClass, img1Src, img2Src, img3Src) => {
-    nav.style.backgroundColor = `${navBgInRGB}`
-    textBox.style.backgroundColor = `${textBoxBgInRGB}`
-    toggleIcon.children[0].textContent = `${modeName}`
-    toggleIcon.children[1].classList.remove(`${removeClass}`)
-    toggleIcon.children[1].classList.add(`${addClass}`)
-    image1.src = `${img1Src}`
-    image2.src = `${img2Src}`
-    image3.src = `${img3Src}`
+// dark or light images
+imageMode = (color) => {
+    image1.src = `img/undraw_proud_coder_${color}.svg`
+    image2.src = `img/undraw_feeling_proud_${color}.svg`
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`
 }
 
 // dark mode
 darkMode = () => {
-    whatMode(
-        `rgb(0 0 0 / 50%)`, 
-        `rgb(255 255 255 / 50%)`,
-        `Dark Mode`,
-        `fa-sun`,
-        `fa-moon`,
-        `img/undraw_proud_coder_dark.svg`,
-        `img/undraw_feeling_proud_dark.svg`,
-        `img/undraw_conceptual_idea_dark.svg`
-
-    )
+    nav.style.backgroundColor = "rgb(0 0 0 / 50%)"
+    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)"
+    toggleIcon.children[0].textContent = "Dark Mode"
+    toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    imageMode("dark")
 }
 lightMode = () => {
     nav.style.backgroundColor = "rgb(255 255 255 / 50%)"
     textBox.style.backgroundColor = "rgb(0 0 0 / 50%)"
     toggleIcon.children[0].textContent = "Light Mode"
-    toggleIcon.children[1].classList.remove("fa-moon")
-    toggleIcon.children[1].classList.add("fa-sun")
-    image1.src = "img/undraw_proud_coder_light.svg"
-    image2.src = "img/undraw_feeling_proud_light.svg"
-    image3.src = "img/undraw_conceptual_idea_light.svg"
+    toggleIcon.children[1].classList.replace("fa-moon", "fa-sun")
+    imageMode("light")
 }
 
 
 switchTheme = (event) => {
-    if (event.target.checked) {
+    if(event.target.checked) {
         document.documentElement.setAttribute("data-theme", "dark")
         darkMode()
     } else {
