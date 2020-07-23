bf4fc1607c968c6d70836fb194a2b6ac3989d794
Chester <chesterdeveloper2000@gmail.com>
2020-07-23 17:26:02 +0800

    fixed the toggle switch

--- script.js
+++ script.js
@@ -52,7 +52,9 @@ console.log(currentTheme)
 
 if (currentTheme) {
     document.documentElement.setAttribute("data-theme", currentTheme)
+
     if (currentTheme === "dark") {
+        toggleSwitch.checked = true
         darkMode()
     }
 }
