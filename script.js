8cf00d913ffc03c9e6e1544f238f1c1d07e754f2
Chester <chesterdeveloper2000@gmail.com>
2020-07-23 17:22:20 +0800

    saved user's preference to local storage.. toggle still has an issue when in dark mode and then refreshing.. the toggle doesn't want to turn on.. gonna fix it later

--- script.js
+++ script.js
@@ -33,12 +33,26 @@ lightMode = () => {
 switchTheme = (event) => {
     if(event.target.checked) {
         document.documentElement.setAttribute("data-theme", "dark")
+        localStorage.setItem("theme", "dark")
         darkMode()
     } else {
         document.documentElement.setAttribute("data-theme", "light")
+        localStorage.setItem("theme", "light")
         lightMode()
     }
 }
 
 // event listener
 toggleSwitch.addEventListener("change", switchTheme)
+
+
+// check local storage for theme
+const currentTheme = localStorage.getItem("theme")
+console.log(currentTheme)
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme)
+    if (currentTheme === "dark") {
+        darkMode()
+    }
+}
