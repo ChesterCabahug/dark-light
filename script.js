776001ea6b8d37d4d3896e828722df2b1211a795
Chester <chesterdeveloper2000@gmail.com>
2020-07-23 17:03:25 +0800

    my method of refactoring.. but ain't gonna finish it.. gonna do the other way

--- script.js
+++ script.js
@@ -7,16 +7,31 @@ const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
 
+// helper func for dark mode
+whatMode = (navBgInRGB, textBoxBgInRGB, modeName, removeClass, addClass, img1Src, img2Src, img3Src) => {
+    nav.style.backgroundColor = `${navBgInRGB}`
+    textBox.style.backgroundColor = `${textBoxBgInRGB}`
+    toggleIcon.children[0].textContent = `${modeName}`
+    toggleIcon.children[1].classList.remove(`${removeClass}`)
+    toggleIcon.children[1].classList.add(`${addClass}`)
+    image1.src = `${img1Src}`
+    image2.src = `${img2Src}`
+    image3.src = `${img3Src}`
+}
+
 // dark mode
 darkMode = () => {
-    nav.style.backgroundColor = "rgb(0 0 0 / 50%)"
-    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)"
-    toggleIcon.children[0].textContent = "Dark Mode"
-    toggleIcon.children[1].classList.remove("fa-sun")
-    toggleIcon.children[1].classList.add("fa-moon")
-    image1.src = "img/undraw_proud_coder_dark.svg"
-    image2.src = "img/undraw_feeling_proud_dark.svg"
-    image3.src = "img/undraw_conceptual_idea_dark.svg"
+    whatMode(
+        `rgb(0 0 0 / 50%)`, 
+        `rgb(255 255 255 / 50%)`,
+        `Dark Mode`,
+        `fa-sun`,
+        `fa-moon`,
+        `img/undraw_proud_coder_dark.svg`,
+        `img/undraw_feeling_proud_dark.svg`,
+        `img/undraw_conceptual_idea_dark.svg`
+
+    )
 }
 lightMode = () => {
     nav.style.backgroundColor = "rgb(255 255 255 / 50%)"
@@ -31,7 +46,7 @@ lightMode = () => {
 
 
 switchTheme = (event) => {
-    if(event.target.checked) {
+    if (event.target.checked) {
         document.documentElement.setAttribute("data-theme", "dark")
         darkMode()
     } else {
